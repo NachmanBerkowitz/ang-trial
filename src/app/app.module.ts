7f074209397adae35c8fef1ab666f230c159cc74
Nachman Berkowitz <nberkowitz@madisontrust.com>
2022-02-15 13:22:54 -0500

    angular styling demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { EmbeddedComponentComponent } from './embedded-component/embedded-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    EmbeddedComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/embedded-component/embedded-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-embedded-component',
+  templateUrl: './embedded-component.component.html',
+  styleUrls: ['./embedded-component.component.css']
+})
+export class EmbeddedComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
